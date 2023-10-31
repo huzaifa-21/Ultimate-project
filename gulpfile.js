7076a6e8ed030a9effce59163770f3551b55bf93
7ozaifa <huzaifasalah9@gmail.com>
2023-10-31 12:39:36 +0200

    my first destructure

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require("gulp"),
+  concat = require("gulp-concat"),
+  prefix = require("gulp-autoprefixer"),
+  sass = require('gulp-sass')(require("sass")),
+  connect = require('gulp-connect'),
+  pug = require('gulp-pug')
+  ;
+
+// Task html 
+gulp.task('html', async () => {
+  return gulp.src('project/html/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+// Task css
+gulp.task("css", function () {
+  return gulp.src(['project/css/main.scss',])
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(prefix('last 2 versions'))
+    .pipe(gulp.dest('dist'));
+});
+
+// javascript Task 
+gulp.task("js", async function () {
+  return gulp.src('project/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+// Task watch
+gulp.task('watch', () => {
+  connect.server({
+    port: 8888,
+    liveReload: true
+  });
+  gulp.watch('project/html/*.pug', gulp.parallel('html'));
+  gulp.watch('project/css/**/*.*', gulp.parallel('css'));
+});
+
